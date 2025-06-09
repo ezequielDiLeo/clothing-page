a1461dcb0981c3504bc83b449d39279cb3296cb2
Ezequiel Di Leo <ezemr1996@gmail.com>
2025-06-09 12:21:16 -0300

    Agrego componente ItemDetail

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { ItemDetail } from '../itemDetail/itemDetail'
+import { ItemDetail } from '../itemDetail/ItemDetail'
 import { db } from "../../firebase/config"
 import { doc, getDoc } from 'firebase/firestore' 
 
